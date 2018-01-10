bb6d7c3ca21001a9c25cd3a80de428b0c851ebdf
Soongja <david0801@naver.com>
2018-01-10 20:30:13 +0900

    add acgan model

--- ACGAN/models/acgan.py
+++ ACGAN/models/acgan.py
@@ -3,12 +3,15 @@ import torch.nn as nn
 import torch.nn.parallel
 
 class _netG(nn.Module):
-    def __init__(self, ngpu, nz, ngf, nc):
+    def __init__(self, ngpu, nz, nl, ngf, nc):
         super(_netG, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d(     nz, ngf * 8, 4, 1, 0, bias=False),
+            # input is Z
+            nn.Linear(nz + nl, ngf * 16),
+            nn.ReLU(True),
+            # state size. (ngf*16) x 1 x 1
+            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
@@ -20,11 +23,11 @@ class _netG(nn.Module):
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
@@ -56,17 +59,25 @@ class _netD(nn.Module):
             nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()
-        )
+            # state size. (ndf*8) x 4 x 4)
+            nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True))
+            # state size. (ndf*16) x 1 x 1)
+        self.dis = nn.Sequential(
+            nn.Linear(ndf * 16, 1),
+            nn.Sigmoid())
+        self.aux = nn.Sequential(
+            nn.Linear(ndf * 16, nl),
+            nn.Sigmoid())
 
     def forward(self, input):
         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+            output_dis = nn.paralle.data_parallel(self.dis, output, range(self.ngpu))
+            output_aux = nn.paralle.data_parallel(self.aux, output, range(self.ngpu))
         else:
             output = self.main(input)
-
-        return output.view(-1, 1).squeeze(1)
-
-
+            output_dis = self.dis(output)
+            output_aux = self.aux(output)
+        return output_dis.view(-1, 1).squeeze(1), output_aux.view(-1, 10).squeeze(1)
