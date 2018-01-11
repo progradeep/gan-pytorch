bc90f3d2d62dc1fdc92f30c59e3b8d7b7cd34c09
Soongja <david0801@naver.com>
2018-01-12 07:56:57 +0900

    ACGAN for cifar10

--- ACGAN/models/acgan.py
+++ ACGAN/models/acgan.py
@@ -2,46 +2,44 @@ import torch
 import torch.nn as nn
 import torch.nn.parallel
 
-
 class _netG(nn.Module):
-    def __init__(self, ngpu, nz, nl, ngf, nc):
+    def __init__(self, ngpu, nz, ngf, nc):
         super(_netG, self).__init__()
         self.ngpu = ngpu
         self.ngf = ngf
-        self.fc = nn.Sequential(
-            # input is Z
-            nn.Linear(nz + nl, ngf * 8 * 4 * 4),
-            nn.ReLU(True))
-        self.deconv = nn.Sequential(
-            # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+        self.nz = nz
+
+        self.fc = nn.Linear(nz, ngf * 8)
+        self.tconv = nn.Sequential(
+            # state size. (ngf*16)
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
-            # state size. (ngf*8) x 8 x 8
+            # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
+            # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
+            # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
-            nn.Tanh())
-            # state size. (nc) x 64 x 64
+            nn.Tanh()
+            # state size. (ngf) x 32 x 32
+            )
 
-
-    def forward(self, input, label):
+    def forward(self, input):
         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
-            output = torch.cat((input, label), 1)
-            output = nn.parallel.data_parallel(self.fc(output), output, range(self.ngpu))
-            output = output.view(-1, self.ngf * 8, 4, 4)
-            output = nn.parallel.data_parallel(self.deconv(output), output, range(self.ngpu))
+            input = input.view(-1, self.nz)
+            output = nn.parallel.data_parallel(self.fc, input, range(self.ngpu))
+            output = output.view(-1, self.ngf * 8, 1, 1)
+            output = nn.parallel.data_parallel(self.tconv, output, range(self.ngpu))
         else:
-            output = torch.cat((input, label), 1)
-            output = self.fc(output)
-            output = output.view(-1, self.ngf * 8, 4, 4)
-            output = self.deconv(output)
+            input = input.view(-1, self.nz)
+            output = self.fc(input)
+            output = output.view(-1, self.ngf * 8, 1, 1)
+            output = self.tconv(output)
         return output
 
 
@@ -50,48 +48,57 @@ class _netD(nn.Module):
         super(_netD, self).__init__()
         self.ngpu = ngpu
         self.ndf = ndf
-        self.nl = nl
-        self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+
+        self.conv = nn.Sequential(
+            # input is (nc) x 32 x 32
+            nn.Conv2d(nc, ndf, 3, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.Dropout(0.5, inplace=False),
+            # state size. (ndf) x 16 x 16
+            nn.Conv2d(ndf, ndf * 2, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5, inplace=False),
             # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf * 2, ndf * 4, 3, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5, inplace=False),
             # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.Conv2d(ndf * 4, ndf * 8, 3, 1, 1, bias=False),
             nn.BatchNorm2d(ndf * 8),
-            nn.LeakyReLU(0.2, inplace=True))
-            # state size. (ndf*8) x 4 x 4)
-
-        self.fc = nn.Sequential(
-            nn.Linear(ndf * 8 * 4 * 4, ndf * 16),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5, inplace=False),
+            # state size. (ndf*8) x 8 x 8
+            nn.Conv2d(ndf * 8, ndf * 16, 3, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 16),
-            nn.LeakyReLU(0.2, inplace=True))
-        # state size. (ndf*16) x 1 x 1)
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5, inplace=False),
+            # state size. (ndf*16) x 4 x 4
+            nn.Conv2d(ndf * 16, ndf * 32, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(ndf * 32),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5, inplace=False)
+            # state size. (ndf*32) x 4 x 4
+            )
         self.dis = nn.Sequential(
-            nn.Linear(ndf * 16, 1),
-            nn.Sigmoid())
+            nn.Linear(ndf * 32 * 4 * 4, 1),
+            nn.Sigmoid()
+            )
         self.aux = nn.Sequential(
-            nn.Linear(ndf * 16, nl),
+            nn.Linear(ndf * 32 * 4 * 4, nl),
             nn.Softmax()
             )
 
     def forward(self, input):
         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-            output = output.view(-1, self.ndf * 16)
-            output_dis = nn.paralle.data_parallel(self.dis, output, range(self.ngpu))
-            output_aux = nn.paralle.data_parallel(self.aux, output, range(self.ngpu))
+            output = nn.parallel.data_parallel(self.conv, input, range(self.ngpu))
+            output = output.view(-1, self.ndf * 32 * 4 * 4)
+            dis_output = nn.parallel.data_parallel(self.dis, output, range(self.ngpu)).view(-1, 1).squeeze(1)
+            aux_output = nn.parallel.data_parallel(self.aux, output, range(self.ngpu))
         else:
-            output = self.main(input)
-            output = output.view(-1, self.ndf * 8 * 4 * 4)
-            output = self.fc(output)
-            output_dis = self.dis(output)
-            output_aux = self.aux(output)
-        return output_dis.squeeze(), output_aux.squeeze()
+            output = self.conv(input)
+            output = output.view(-1, self.ndf * 32 * 4 * 4)
+            dis_output = self.dis(output).view(-1, 1).squeeze(1)
+            aux_output = self.aux(output)
+        return dis_output, aux_output
--- ACGAN/trainer.py
+++ ACGAN/trainer.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.utils as vutils
+import numpy as np
 from torch.autograd import Variable
 
 import models.acgan as acgan
@@ -47,7 +48,7 @@ class Trainer(object):
             self.netG.cuda()
 
     def build_model(self):
-        self.netG = acgan._netG(self.ngpu, self.nz, self.nl, self.ngf, self.nc)
+        self.netG = acgan._netG(self.ngpu, self.nz, self.ngf, self.nc)
         self.netG.apply(weights_init)
         if self.config.netG != '':
             self.netG.load_state_dict(torch.load(self.config.netG))
@@ -57,27 +58,28 @@ class Trainer(object):
             self.netD.load_state_dict(torch.load(self.config.netD))
 
     def train(self):
-        criterion = nn.BCELoss()
-        cross_entropy_loss = nn.CrossEntropyLoss()  # add class loss
+        dis_criterion = nn.BCELoss()
+        aux_criterion = nn.NLLLoss()  # add class loss
 
         input = torch.FloatTensor(self.batch_size, 3, self.image_size, self.image_size)
-        noise = torch.FloatTensor(self.batch_size, self.nz)
-        fixed_noise = torch.FloatTensor(self.batch_size, self.nz).normal_(0, 1)
-        noise2 = torch.FloatTensor(self.batch_size, self.nl)
-        fixed_noise2 = torch.FloatTensor(self.batch_size, self.nl).normal_(0, 1)
-        fixed_noise2 = torch.nn.functional.softmax(Variable(fixed_noise2))
-        class_label = torch.LongTensor(self.batch_size)  # add class label
-        label = torch.FloatTensor(self.batch_size)
+        noise = torch.FloatTensor(self.batch_size, self.nz, 1, 1)
+        fixed_noise = torch.FloatTensor(self.batch_size, self.nz, 1, 1).normal_(0, 1)
+        dis_label = torch.FloatTensor(self.batch_size)
+        aux_label = torch.LongTensor(self.batch_size) # add class label
         real_label = 1
         fake_label = 0
 
         if self.cuda:
-            criterion.cuda()
-            cross_entropy_loss.cuda()
-            input, label, class_label = input.cuda(), label.cuda(), class_label.cuda()  # add class label
+            dis_criterion.cuda()
+            aux_criterion.cuda()
+            input, dis_label, aux_label = input.cuda(), dis_label.cuda(), aux_label.cuda()  # add class label
             noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
 
-        fixed_noise = Variable(fixed_noise)
+        inputv = Variable(input)
+        noisev = Variable(noise)
+        fixed_noisev = Variable(fixed_noise)
+        dis_labelv = Variable(dis_label)
+        aux_labelv = Variable(aux_label)
 
         # setup optimizer
         optimizerD = optim.Adam(self.netD.parameters(), lr=self.lr, betas=(self.beta1, 0.999))
@@ -93,44 +95,45 @@ class Trainer(object):
 
                 # train with real
                 self.netD.zero_grad()
-                real_cpu, c_label = data  # add c label
+                real_cpu, c_label = data  # add c_label
                 batch_size = real_cpu.size(0)
                 if self.cuda:
                     real_cpu = real_cpu.cuda()
                     c_label = c_label.cuda()  # add c label
-                input.resize_as_(real_cpu).copy_(real_cpu)
-                label.resize_(batch_size).fill_(real_label)
-                class_label = c_label
+                inputv.data.resize_as_(real_cpu).copy_(real_cpu)
+                dis_labelv.data.resize_(batch_size).fill_(real_label)
+                aux_labelv.data.resize_(batch_size).copy_(c_label)
 
-                inputv = Variable(input)
-                labelv = Variable(label)
-                class_labelv = Variable(class_label)
+                dis_out, aux_out = self.netD(inputv)
+                dis_errD_real = dis_criterion(dis_out, dis_labelv)
+                aux_errD_real = aux_criterion(aux_out, aux_labelv)
+                errD_real = dis_errD_real + aux_errD_real
+                errD_real.backward()
 
-                out1, out2 = self.netD(inputv)
-                errD_real = criterion(out1, labelv)
-                errC_real = cross_entropy_loss(out2, class_labelv)
-
-                D_x = out1.data.mean()
+                D_x = dis_out.data.mean()
 
                 # train with fake
-                noise.resize_(batch_size, self.nz).normal_(0, 1)
-                noisev = Variable(noise)
-                noise2.resize_(batch_size, self.nl).normal_(0, 1)
-                noise2v = torch.nn.functional.softmax(Variable(noise2))
+                noisev.data.resize_(batch_size, self.nz, 1, 1).normal_(0, 1)
+                c_label = np.random.randint(0, self.nl, batch_size)
+                noisev_ = np.random.normal(0, 1, (batch_size, self.nz))
+                class_onehot = np.zeros((batch_size, self.nl))
+                class_onehot[np.arange(batch_size), c_label] = 1
+                noisev_[np.arange(batch_size), :self.nl] = class_onehot[np.arange(batch_size)]
+                noisev_ = (torch.from_numpy(noisev_))
+                noisev.data.copy_(noisev_.view(batch_size, self.nz, 1, 1))
+                aux_labelv.data.resize_(batch_size).copy_(torch.from_numpy(c_label))
 
+                fake = self.netG(noisev)
+                dis_labelv.data.fill_(fake_label)
+                dis_out, aux_out = self.netD(fake.detach())
+                dis_errD_fake = dis_criterion(dis_out, dis_labelv)
+                aux_errD_fake = aux_criterion(aux_out, aux_labelv)
+                errD_fake = dis_errD_fake + aux_errD_fake
+                errD_fake.backward()
 
-                fake = self.netG(noisev, noise2v)
-                labelv = Variable(label.fill_(fake_label))
-                out1, out2 = self.netD(fake.detach())
-                errD_fake = criterion(out1, labelv)
-                errC_fake = cross_entropy_loss(out2, class_labelv)
+                D_G_z1 = dis_out.data.mean()
 
-                D_G_z1 = out1.data.mean()
-
-                errD_D = errD_real + errD_fake
-                errD_C = errC_real + errC_fake  # add errC
-                err = errD_D + errD_C
-                err.backward()
+                errD = errD_real + errD_fake
                 optimizerD.step()
 
                 ############################
@@ -139,31 +142,30 @@ class Trainer(object):
                 for p in self.netD.parameters():
                     p.requires_grad = False  # to avoid computation
                 self.netG.zero_grad()
-                labelv = Variable(label.fill_(real_label))  # fake labels are real for generator cost
-                out1, out2 = self.netD(fake)
+                dis_labelv.data.fill_(real_label) # fake labels are real for generator cost
+                dis_out, aux_out = self.netD(fake)
 
-                errG_D = criterion(out1, labelv)
-                errG_C = cross_entropy_loss(out2, class_labelv)
-                err = errG_D + errG_C
+                dis_errG = dis_criterion(dis_out, dis_labelv)
+                aux_errG = aux_criterion(aux_out, aux_labelv)
+                errG = dis_errG + aux_errG
+                errG.backward()
 
-                D_G_z2 = out1.data.mean()
-
-                err.backward()
+                D_G_z2 = dis_out.data.mean()
                 optimizerG.step()
 
                 print(
-                    '[%d/%d][%d/%d] Loss_D_D: %.4f Loss_D_C: %.4f  Loss_G_D: %.4f  Loss_G_C: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-                    % (epoch, self.niter, i, len(self.data_loader),
-                       errD_D.data[0], errD_C.data[0], errG_D.data[0], errG_C.data[0], D_x, D_G_z1, D_G_z2))
-                if i % 1 == 0:
+                    '[%d/%d][%d/%d] Loss_D_real: %.4f Loss_D_fake: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+                    % (epoch+1, self.niter, i+1, len(self.data_loader),
+                       errD_real.data[0], errD_fake.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
+                if i % 100 == 0:
                     vutils.save_image(real_cpu,
                                       '%s/real_samples.png' % self.outf,
                                       normalize=True)
-                    fake = self.netG(fixed_noise, fixed_noise2)
+                    fake = self.netG(fixed_noisev)
                     vutils.save_image(fake.data,
-                                      '%s/fake_samples_epoch_%03d.png' % (self.outf, epoch),
+                                      '%s/fake_samples_epoch_%03d.png' % (self.outf, epoch+1),
                                       normalize=True)
 
             # do checkpointing
-            torch.save(self.netG.state_dict(), '%s/netG_epoch_%03d.pth' % (self.outf, epoch))
-            torch.save(self.netD.state_dict(), '%s/netD_epoch_%03d.pth' % (self.outf, epoch))
+            torch.save(self.netG.state_dict(), '%s/netG_epoch_%03d.pth' % (self.outf, epoch+1))
+            torch.save(self.netD.state_dict(), '%s/netD_epoch_%03d.pth' % (self.outf, epoch+1))
