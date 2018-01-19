b94c6a2b7d6e81292400427bae814785f0f31bab
lllldoodoo <cksdnd619@naver.com>
2018-01-19 13:38:12 +0900

    .

--- pix2pix/test.py
+++ pix2pix/test.py
@@ -7,18 +7,20 @@ from torch.autograd import Variable
 import torchvision.transforms as transforms
 
 from data_loader import is_image_file, load_img, save_img
+from models import pix2pix
 
 # Testing settings
 parser = argparse.ArgumentParser(description='pix2pix-PyTorch-implementation')
-parser.add_argument('--dataset', required=True, help='facades')
-parser.add_argument('--model', type=str, default='checkpoint/facades/netG_model_epoch_200.pth', help='model file to use')
+
+parser.add_argument('--model', type=str, default='samples/netG_epoch_200.pth', help='model file to use')
 parser.add_argument('--cuda', action='store_true', help='use cuda')
 opt = parser.parse_args()
-print(opt)
 
-netG = torch.load(opt.model)
 
-image_dir = "dataset/{}/test/a/".format(opt.dataset)
+netG = pix2pix.define_G(3, 3, 64, 'batch', False, [])
+netG.load_state_dict(torch.load(opt.model))
+
+image_dir = "datasets/test/a/"
 image_filenames = [x for x in os.listdir(image_dir) if is_image_file(x)]
 
 transform_list = [transforms.ToTensor(),
@@ -38,6 +40,6 @@ for image_name in image_filenames:
     out = netG(input)
     out = out.cpu()
     out_img = out.data[0]
-    if not os.path.exists(os.path.join("result", opt.dataset)):
-        os.mkdir(os.path.join("result", opt.dataset))
-    save_img(out_img, "result/{}/{}".format(opt.dataset, image_name))
+    if not os.path.exists("datasets/result"):
+        os.mkdir("datasets/result")
+    save_img(out_img, "datasets/result/{}".format(image_name))
